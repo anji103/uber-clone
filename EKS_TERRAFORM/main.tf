8774746ae8a0d6f1a618dae9a2843455887f1323
anji103 <150421715+anji103@users.noreply.github.com>
2024-09-30 14:33:51 +0530

    Update main.tf

--- EKS_TERRAFORM/main.tf
+++ EKS_TERRAFORM/main.tf
@@ -42,4 +42,16 @@ resource "aws_iam_role" "eks_role" {
     }]
   })
 }
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
 

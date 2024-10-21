6e86586d6233e8b8a79c74f0fe3fe7ce74b1dc98
anji103 <150421715+anji103@users.noreply.github.com>
2024-10-21 13:13:35 +0530

    Update main.tf

--- EKS_TERRAFORM/main.tf
+++ EKS_TERRAFORM/main.tf
@@ -3,7 +3,7 @@ resource "aws_eks_cluster" "my_cluster" {
   role_arn = "arn:aws:iam::060795910502:role/anji-role"  # Replace with your IAM role ARN
 
   vpc_config {
-    subnet_ids         = ["subnet-0e5a3ae1ed0820619", "subnet-0f73c0829609a5147"]  # Replace with your subnet IDs
+    subnet_ids         = ["subnet-03e6aaed9d64539fe", "subnet-0f73c0829609a5147"]  # Replace with your subnet IDs
     security_group_ids = ["sg-0a00b3e4ff192284b"]                         # Replace with your security group IDs
   }
 
@@ -16,7 +16,7 @@ resource "aws_eks_node_group" "my_node_group" {
   cluster_name    = aws_eks_cluster.my_cluster.name
   node_group_name = "my-node-group"
   node_role_arn   = "arn:aws:iam::060795910502:role/anji-role"  # Replace with your IAM role ARN
-  subnet_ids         = ["subnet-0e5a3ae1ed0820619", "subnet-0f73c0829609a5147"]      # Replace with your subnet IDs
+  subnet_ids         = ["subnet-03e6aaed9d64539fe", "subnet-0f73c0829609a5147"]      # Replace with your subnet IDs
 
   scaling_config {
     desired_size = 1
